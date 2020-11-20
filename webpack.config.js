5d22d0c37973069a6930c665c2b4b0fc650a0d85
Omar Montoya <43155049+imremain@users.noreply.github.com>
2020-11-20 10:52:01 -0800

    Transformed app to JWT

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,43 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  output: {
-    filename: 'app.bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ],
-  module:{
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+              test: /\.css$/,
+              use: [
+                "style-loader",
+                { loader: "css-loader", options: { importLoaders: 1 } },
+                "postcss-loader",
+              ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
         }
-      }
-    ]
-  }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
 }

37091b974d6bd634179e6d2fdcd9bbcce83fd17c
Omar Montoya <43155049+imremain@users.noreply.github.com>
2020-11-23 11:47:27 -0800

    Added Vercel to new build

--- webpack.config.js
+++ webpack.config.js
@@ -23,21 +23,24 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@': path.resolve(__dirname, 'src/'),
-        }
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './public/index.html'
-    })],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
-        })
-    }
+      extensions: ['.js', '.jsx'],
+      alias: {
+          '@': path.resolve(__dirname, 'src/'),
+      }
+  },
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './public/index.html'
+  })],
+  devServer: {
+      historyApiFallback: true
+  },
+  externals: {
+      // global app config object
+      config: JSON.stringify({
+          apiUrl: 'http://localhost:4000'
+      })
+  }
 }

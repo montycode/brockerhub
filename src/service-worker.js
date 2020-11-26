5ea7605648021d6cbe5d23c435161e3318453d5a
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2020-11-25 21:12:29 -0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,3 +3,8 @@ const reactAppRewireWorkbox = require("react-app-rewire-workbox")
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 reactAppRewireWorkbox.precaching.suppressWarning()
 reactAppRewireWorkbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+// App shell
+workbox.routing.registerNavigationRoute(
+    workbox.precaching.getCacheKeyForURL('./index.html')
+  );

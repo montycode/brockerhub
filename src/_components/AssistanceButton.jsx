a46dd2c9d35ab950c0d5a4d311407565227f5a70
Omar Montoya <43155049+imremain@users.noreply.github.com>
2020-12-04 11:15:04 -0800

    Update AssistanceButton.jsx

--- src/_components/AssistanceButton.jsx
+++ src/_components/AssistanceButton.jsx
@@ -5,7 +5,7 @@ export const AssistButton = () => {
     return(
         <div className="assist_button">
             <div className="help_button uppercase text-center bg-orange text-white rounded-sm p-3">
-                <a href="tel:+526644079655">
+                <a href="tel:+526641990980">
                     <div className="flex flex-col justify-center">
                         <AssistanceIcon classNames={'fill-current text-white w-6 mx-auto'}/>
                         <p className='text-xxs font-bold'>Asistencia</p>

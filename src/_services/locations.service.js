869a95b037db6cf095164b642b9664f6b40edfb5
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2020-12-09 19:40:25 -0800

    Added LocationServices

---
+++ src/_services/locations.service.js
@@ -0,0 +1,27 @@
+import config from 'config'
+import { authHeader, handleResponse } from '@/_helpers'
+
+export const locationsService = {
+    getLocations,
+    getSingleLocation
+};
+
+function getLocations() {
+    const requestOptions = { method: 'GET', headers: authHeader() };
+    return fetch(`${config.apiUrl}/locations`, requestOptions)
+    .then(response => response.json())
+    .then(locations =>{
+        return locations;
+    });
+}
+
+function getSingleLocation(id) {
+    const requestOptions = { method: 'GET', headers: authHeader() };
+    return fetch(`${config.apiUrl}/locations`, requestOptions)
+    .then(response => response.json())
+    .then(location => {
+        location = location.results[id-1];
+        console.log(location);
+        return location;
+    });
+}

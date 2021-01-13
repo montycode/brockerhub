9cfad5b49499acc9b965ca82a50f127ba5dec959
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2021-01-12 19:31:39 -0800

    Update Policy Fetch

--- src/_helpers/handle-login.js
+++ src/_helpers/handle-login.js
@@ -4,11 +4,13 @@ export function handleLogin(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
         if (!response.ok) {
-            data.message = 'Usuario y/o contraseña incorrectos.';
+            if (response.status === 401) {
+                data.message = 'Usuario y/o contraseña incorrectos.';
+            }
             if (response.status === 403) {
                 data.message = 'Debes confirmar tu correo electronico para ingresar';
             }
-            const error = (data && data.message);
+            const error = (data && data.message) || response.statusText;
             return Promise.reject(error);
         }
 
